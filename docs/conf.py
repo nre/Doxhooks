6b16b2137997dc44a397796a5b06edd6039074ea
Nick Evans <nick.evans3976@gmail.com>
2015-06-02 14:45:18 +0100

    Revert docs LaTeX preamble and max list depth
    This reverts commits 2820, 4afd1 and f5be.
    
    ReadTheDocs PDF builds fail with "! LaTeX Error: Too deeply nested.".
    Two of these errors are fixed by adding the LaTeX preamble:
    
        \usepackage{enumitem}
        \setlistdepth{10}
    
    But increasing the list depth to 1000 did not fix a third error.
    Monkeypatching the definition of Verbatim (from sphinx.sty) in the
    preamble also did not fix the error.
    
    sphinx-doc/sphinx#777
    
    Monkeypatch:
    http://trac.sagemath.org/ticket/9107#comment:75
    http://git.sagemath.org/sage.git/commit/?id=891c3fad654e89e6b96bcf8f79114f631c8b7bba

--- docs/conf.py
+++ docs/conf.py
@@ -42,10 +42,6 @@ html_use_smartypants = True
 html_domain_indices = False  # If True, see modindex_common_prefix.
 html_show_sourcelink = False
 
-latex_elements = {
-    "preamble": "\\usepackage{enumitem}\n\\setlistdepth{50}",
-}
-
 
 extensions = [
     "sphinx.ext.autodoc",
